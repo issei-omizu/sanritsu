2b0fbdb8b88ccff3b5e8da27ae74136292a8a96b
issei omizu <p.solberg2003@gmail.com>
2019-05-15 13:10:08 +0900

    画像とか差し替え

--- js/main.js
+++ js/main.js
@@ -78,6 +78,17 @@
 	    });
 		});
 
+		$(document).on('click', '#ftco-links a[href^="#"]', function (event) {
+			event.preventDefault();
+
+			var href = $.attr(this, 'href');
+
+			$('html, body').animate({
+				scrollTop: $($.attr(this, 'href')).offset().top - 70
+			}, 500, function() {
+				// window.location.hash = href;
+			});
+		});
 	};
 
 	onePageClick();
